41434de136aecd684bb1787aea8683eedf5a2985
Learning Bots <115718228+Learningbots79@users.noreply.github.com>
2024-10-15 22:35:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,14 +15,14 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '21484575'))
 API_HASH = environ.get('API_HASH', 'db6286a041a873eb6c571df07ef4244e')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7794152832:AAGKrqIkIGGPiwkauHePl-TVMKWBlSuWrRA')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7807282657:AAHFABI9ygow9sLqNQuI7hsWPm5FLNfBr9c')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002429935242'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002190681742'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002429935242').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002190681742').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://learningbots:learningbots@learningbots.5yddr.mongodb.net/?retryWrites=true&w=majority&appName=learningbots")
