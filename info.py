2e1414ed1c22ea2106995f6cf433245c2a793a44
PRADEEP74384 <pradeepkagra156@gmail.com>
2025-01-15 01:52:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,14 +18,14 @@ API_HASH = environ.get('API_HASH', 'f8b1697bee0a94643d9f28c67af49ab1')
 BOT_TOKEN = environ.get('BOT_TOKEN', '8012699592:AAH2D7y37YpzLh-ReE0zVdhGUqNJN1zWDGc')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1001944607209').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6195556018').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Infinity80554") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002264090025'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+V7c6rfdKuBphZWQ9')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002320082277').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv:mongodb+srv://Hustle656:Hustle656@hustle656.0xlot.mongodb.net/?retryWrites=true&w=majority&appName=Hustle656")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Hustle656:Hustle656@hustle656.0xlot.mongodb.net/?retryWrites=true&w=majority&appName=Hustle656")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Hustle656")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
